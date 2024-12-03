61ed703aaff3f88a34f48deeff592a78443cb67a
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-03 18:10:04 -0300

    feat: Adiciona prompts para entrada de dois números

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        {
 
+            // Definindo as variáveis para os números
+            System.out.print("Digite o primeiro número: ");
+            double numero1 = entrada.nextDouble();
+
+            System.out.print("Digite o segundo número: ");
+            double numero2 = entrada.nextDouble();
+
+        }
     }
 }
